ec2b56eeab15def379d9d872fdd41ec8f2862dac
leyvapaco <82705842+leyvapaco@users.noreply.github.com>
2025-10-22 09:02:42 +0200

    .

--- src/ud1/tarea5/Ej5_4.java
+++ src/ud1/tarea5/Ej5_4.java
@@ -1,9 +1,9 @@
-package ud1.tarea5b;
+package ud1.tarea5;
 import java.util.Scanner;
 
-//Solución a Ej5 sando bucles
+//Solución a Ej5b sin usar bucles
 
-public class Ej5b_4b {
+public class Ej5_4 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -14,21 +14,18 @@ public class Ej5b_4b {
 		
 		final int HORAS_SEMANA=40; //constante
 		
+		int excesoHoras=(horasTrabajadas-HORAS_SEMANA>0)?horasTrabajadas-HORAS_SEMANA:0;
+		
 		float precioHoraOrdinaria=12.0f;
 		float precioHoraExtra=16.0f;
 		
 		float salarioSemanal=0.0f;
 		
-		int contador=0;
-		while (contador<horasTrabajadas) {
-			if (contador<HORAS_SEMANA)
-				salarioSemanal=salarioSemanal+precioHoraOrdinaria;
-			else
-				salarioSemanal=salarioSemanal+precioHoraExtra;
-			contador++;
-		}
+		salarioSemanal=(horasTrabajadas-excesoHoras)*precioHoraOrdinaria;
+		salarioSemanal+=excesoHoras*precioHoraExtra;
+
 		
-		System.out.println("El sueldo por trabajar "+horasTrabajadas+" es "+salarioSemanal+" euros.");
+		System.out.println("El sueldo por trabajar "+horasTrabajadas+" horas es "+salarioSemanal+" euros.");
    }
 
 
--- src/ud1/tarea5/Ej5b_4b.java
+++ src/ud1/tarea5/Ej5b_4b.java
@@ -1,9 +1,10 @@
-package ud1.tarea5b;
+package ud1.tarea5
+;
 import java.util.Scanner;
 
-//Solución a Ej5b sin usar bucles
+//Solución a Ej5 usando bucles
 
-public class Ej5b_4 {
+public class Ej5b_4b {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -14,18 +15,21 @@ public class Ej5b_4 {
 		
 		final int HORAS_SEMANA=40; //constante
 		
-		int excesoHoras=(horasTrabajadas-HORAS_SEMANA>0)?horasTrabajadas-HORAS_SEMANA:0;
-		
 		float precioHoraOrdinaria=12.0f;
 		float precioHoraExtra=16.0f;
 		
 		float salarioSemanal=0.0f;
 		
-		salarioSemanal=(horasTrabajadas-excesoHoras)*precioHoraOrdinaria;
-		salarioSemanal+=excesoHoras*precioHoraExtra;
-
+		int contador=0;
+		while (contador<horasTrabajadas) {
+			if (contador<HORAS_SEMANA)
+				salarioSemanal=salarioSemanal+precioHoraOrdinaria;
+			else
+				salarioSemanal=salarioSemanal+precioHoraExtra;
+			contador++;
+		}
 		
-		System.out.println("El sueldo por trabajar "+horasTrabajadas+" horas es "+salarioSemanal+" euros.");
+		System.out.println("El sueldo por trabajar "+horasTrabajadas+" es "+salarioSemanal+" euros.");
    }
 
 
