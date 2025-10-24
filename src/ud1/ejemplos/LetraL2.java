08ec41f5d11e2066e2c1f2c5805ea23d03a09c63
leyvapaco <82705842+leyvapaco@users.noreply.github.com>
2025-10-24 18:56:27 +0200

    Ejemplo de bucle for anidado. Pintamos una letra

---
+++ src/ud1/ejemplos/LetraL2.java
@@ -0,0 +1,11 @@
+package ud1.ejemplos;
+
+public class LetraL2 {
+	public static void main(String[] args) {
+		
+		for (int i=0;i<5;i++)
+			System.out.println("*"); 
+			for (int j=0;j<5;j++)
+				System.out.print("*");
+	}
+}
