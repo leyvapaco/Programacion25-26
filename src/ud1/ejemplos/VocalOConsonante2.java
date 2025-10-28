5b81c9b93ed77ed5bf92594e9946970da9e4206b
leyvapaco <82705842+leyvapaco@users.noreply.github.com>
2025-10-28 19:26:38 +0100

    CReo una segunda versión de prueba

---
+++ src/ud1/ejemplos/VocalOConsonante2.java
@@ -0,0 +1,27 @@
+package ud1.ejemplos;
+
+import java.util.Scanner;
+
+public class VocalOConsonante2 {
+public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		char letra=sc.next().toLowerCase().charAt(0);
+		
+		System.out.println("Introduce una letra");
+		
+		if (Character.isLetter(letra)){ //validamos que efectivamente sea una letra
+		
+			switch (letra) {
+				case 'a','e','i','o','u':
+					System.out.println("Es una vocal");
+				break;
+				default: 
+					System.out.println("Es una consonante");
+				
+			}
+		}
+		else 
+			System.out.println("Debes introducir una letra");
+	}
+}
