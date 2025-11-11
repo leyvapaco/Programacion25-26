724b3049f3773f0f98cf2cd9136dc536f819d14c
leyvapaco <82705842+leyvapaco@users.noreply.github.com>
2025-11-11 16:54:24 +0100

    Plantilla para modificar código añadiendo nueva excepción

--- src/ud3/ejemplos/Utilidades.java
+++ src/ud3/ejemplos/Utilidades.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 
 public class Utilidades {
 	
-static boolean validaOperacion(Character op) {
-		return true; //Ejercicio: Modificar para que compruebe que la operación no es dígito ni letra
-	 }
-			
+static boolean validaOperacion(Character op) throws OpNoValidaException{
 	
+	if (Character.isLetterOrDigit(op)) {
+		throw new OpNoValidaException("Op no válida");
+		}
+	return true; //Ejercicio: Modificar para que compruebe que la operación no es dígito ni letra
+}
+
+static boolean validaOperando(Character op) throws OperandoNoValidoException{
+ //Ejercicio: Modificar para que compruebe que la operación no es dígito ni letra
+	return(true);
+
+}
+	
+				
  static double suma(double a, double b) {
 	 return a+b;
  }
@@ -33,7 +43,7 @@ static boolean validaOperacion(Character op) {
  
   
  static double calcular(double a, double b, char op) throws DivPorCeroException,RestaException {
-     switch (op) {
+     	 switch (op) {
          case '+': return suma(a,b);
          case '-': return resta(a,b);
          case '*': return producto(a,b);
