737b5a959035298223f13b623497d3160cf3f9ca
leyvapaco <82705842+leyvapaco@users.noreply.github.com>
2026-02-13 13:22:24 +0100

    Añado comentario en el comparador

--- src/ud5/tarea2/CompararTrianguloPorColor.java
+++ src/ud5/tarea2/CompararTrianguloPorColor.java
@@ -8,6 +8,10 @@ public class CompararTrianguloPorColor implements Comparator<Triangulo> {
     @Override
     public int compare(Triangulo t1, Triangulo t2) {
         return t1.color.name().compareTo(t2.color.name());
+        
+        /* Usamos name() porque convierte el literal del enum a String.
+        Así garantizamos un orden alfabético real, incluso si los
+        colores no están declarados en orden alfabético dentro del enum. */
     }
 }
 
